b82ab20a6d148a1e83ea3aedf76d369f7c4fe1e8
Matúš Močkor <codex611@gmail.com>
2023-01-29 16:17:10 +0100

    CHANGED: we not need synchronize db on production

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
         entities: [],
-        synchronize: true,
+        synchronize: configService.get('PRODUCTION'),
       }),
     }),
   ],
